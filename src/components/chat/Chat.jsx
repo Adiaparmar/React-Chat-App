6a9926ca019691b805e63769544da944e5c74aad
adiaparmar <adiaparmar@gmail.com>
2024-05-11 20:16:23 +0530

    Firebase fetching data, Search User and Create User , Fetch Single chat and chat messages

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,15 +1,28 @@
 import { useEffect, useRef, useState } from "react";
 import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../lib/firebase";
 
 const Chat = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
+  const [chat, setChat] = useState();
 
   const endRef = useRef(null)
 
   useEffect(() => {
     endRef.current?.scrollIntoView({behaviour:"smooth"})
+  },[]);
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", "84jWUjNNG1mAyrFMSo4M"),(res) => {
+      setChat(res.data())
+    });
+
+    return() => {
+      unSub();
+    };
   },[]);
 
   const handleEmoji = (e) => {
